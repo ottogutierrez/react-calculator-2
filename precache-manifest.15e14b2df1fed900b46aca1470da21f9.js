c4f3045e4faf7a88e835e3ffe33aeacfdfc15017
Otto Gutierrez <ottogutierrez@Ottos-iMac.phub.net.cable.rogers.com>
2020-07-25 11:18:49 -0400

    Updates

---
+++ precache-manifest.15e14b2df1fed900b46aca1470da21f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4ad155f59a41c4b91a3514181c5469",
+    "url": "/react-calculator-2/index.html"
+  },
+  {
+    "revision": "f6020a7f25f4d4d8e9cb",
+    "url": "/react-calculator-2/static/css/main.61b09068.chunk.css"
+  },
+  {
+    "revision": "f5f081fa0a7fed81ff6f",
+    "url": "/react-calculator-2/static/js/2.5a16d6cc.chunk.js"
+  },
+  {
+    "revision": "f6020a7f25f4d4d8e9cb",
+    "url": "/react-calculator-2/static/js/main.cde6f2a8.chunk.js"
+  },
+  {
+    "revision": "1e3bbc4b42181e54874b",
+    "url": "/react-calculator-2/static/js/runtime~main.723ec01b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41af2ba37a40d445150fd1b7b475a26d.js"
+  "/react-calculator-2/precache-manifest.15e14b2df1fed900b46aca1470da21f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-calculator-2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
